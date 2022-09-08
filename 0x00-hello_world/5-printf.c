da91d0c9a6b4937fdebc9d59cab83eff85af05c3
Japheth Awah <junery115@gmail.com>
2022-09-09 00:45:32 +0100

    using printf to print a statement out

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,9 @@
 /**
  * main - Entry point
  *
- * Description uses printf to print "with proper grammar, but the outcome is a piece of art,"
+ * Description: uses printf to print "with proper grammar,
+ * but the outcome is a piece of art,"
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
