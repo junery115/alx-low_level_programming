23a19380ff4776be7fea3a746005e663c3a03d8d
Japheth Awah <junery115@gmail.com>
2022-09-09 00:42:45 +0100

    using printf to print a statement out

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description uses printf to print "with proper grammar, but the outcome is a piece of art,"
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
