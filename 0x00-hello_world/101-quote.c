7c1fd3573cedcd8bdce1615bd06618005cb1129e
Japheth Awah <junery115@gmail.com>
2022-09-09 01:30:50 +0100

    printing without put of printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,10 +14,9 @@ long syscall = 1;
 long ret = 0;
 
 __asm__("Syscall"
-        : "=a" (ret)
-        : "a" (syscall),
-        "D" (fd),
-        "S" (s),
-        "d" (l));
-    return (1);
+  : "a" (syscall),
+  "D" (fd),
+  "S" (s),
+  "d" (l));
+  return (1);
 }
