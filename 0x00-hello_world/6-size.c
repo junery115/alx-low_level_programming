b24af82d03273e99eb2adfa000608df1939e34f2
Japheth Awah <junery115@gmail.com>
2022-09-09 01:10:27 +0100

    printing sizes of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 /**
-* @c printing size of char
-* @i printing size of integer
-* @lli printing size of long long int
-* @f printing size of float
+* main
 * Description: Printing sizes of types
 *  Return: Always 0 (Success)
 */
 
 int main(void)
 {
-char c;
-int i;
-double d;
-long long int lli;
-float f;
-printf("Size of char: %c", (unsigned long)sizeof(c));
-printf("Size of int: %d", (unsigned long)sizeof(i));
-printf("Size of char: %d", (unsigned long)sizeof(lli));
-printf("Size of char: %f", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of a int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
