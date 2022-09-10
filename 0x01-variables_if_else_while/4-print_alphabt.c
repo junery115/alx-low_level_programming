590bfba2d5597b44bdd50d85621a02fdeb4a26b8
Japheth Awah <junery115@gmail.com>
2022-09-10 02:04:38 +0100

    writing single digit numbers of base 10 from 0

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -22,4 +22,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entrance to code
+ * Description : printing base 10 single dig numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
