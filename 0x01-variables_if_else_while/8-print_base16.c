8fab97f90f839c510a1c09f8098ab5c83626ebe4
Japheth Awah <junery115@gmail.com>
2022-09-10 02:41:56 +0100

    printing hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entrance to code
+ * Description : Write hexadecimal numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int d = '0';
+	while(d <='9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	for (c = 'a'; c < 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
