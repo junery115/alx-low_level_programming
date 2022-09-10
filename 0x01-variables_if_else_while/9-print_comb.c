9afddbed762952528e3b0d39eea1762556305a8c
Japheth Awah <junery115@gmail.com>
2022-09-10 02:56:59 +0100

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entrance to code
+ * Description : printing all combinations of single dig numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
