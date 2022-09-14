361fecbdc3a3c10c1c35c8174c5921e4f6c6ef1f
Japheth Awah <junery115@gmail.com>
2022-09-14 02:55:44 +0100

    printing the last digit of a number

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit
+ * of a number.
+ *
+ * @n: input number as an integer.
+ *
+ *  Return: last digit.
+ */
+
+int print_last_digit(int n)
+{
+
+	int z ;
+
+	if (n < 0)
+	{
+		z = -1 * (n % 10);
+		_putchar(z + '0');
+		return (z);
+	}
+	else
+	{
+		z = n % 10;
+		_putchar(z + '0');
+		return (z);
+	}
+}
