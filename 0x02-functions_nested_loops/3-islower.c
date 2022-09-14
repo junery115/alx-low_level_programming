d2bfda4c37459c40728ba9ae645c0e55fc59d0e7
Japheth Awah <junery115@gmail.com>
2022-09-14 02:17:53 +0100

    Testing if a lower case character is passed to a function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *  _islower(int c) -  test if a character is lower
+ *  _islower -  test if a character is lower
  *  Description: returns 1 if lowercase character and 0 otherwise
- *  @c character to be tested
+ *  @c: character to be tested
  *  Return: 0
  */
 
 int _islower(int c)
 {
-	if (c >=97 && c <= 122 )
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
