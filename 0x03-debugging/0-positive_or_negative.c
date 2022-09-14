7c39d43014d3f17f56a5e76357d10336a9e6f2d3
Japheth Awah <junery115@gmail.com>
2022-09-14 19:00:33 +0100

    another fix

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -9,16 +9,13 @@
 * Return: 0
 *
 */
-int main(void)
+int positive_or_negative(int n)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
 else if (n == 0)
 {
 printf("%d is zero\n", n);
