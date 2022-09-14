b3a745677be5721b8f47efe1dfcfbead23e13cc8
Japheth Awah <junery115@gmail.com>
2022-09-14 01:25:47 +0100

    This function prints _putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,9 @@ int main(void)
 	char ch[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int i;
 
-	for (i = 0; i < sizeof(x); i++)
+	for (i = 0; i < sizeof(ch); i++)
 	{
-		_putchar(x[i]);
+		_putchar(ch[i]);
 	}
 	_putchar('\n');
 	return (0);
