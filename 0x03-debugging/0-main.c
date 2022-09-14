7e4259dffec3d9f526e209c024f7f8569df4b31d
Japheth Awah <junery115@gmail.com>
2022-09-14 19:04:57 +0100

    finally I understand

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - checks that function positive_or_negative prints correct output
- * Description: Given 0 function positive_or_negative must give correct output
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int v;
+	int i;
 
-	v = 0;
-	positive_or_negative(v);
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
-
