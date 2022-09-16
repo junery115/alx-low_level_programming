8a140e5044bb8fc145483161361725c20f4436cb
Japheth Awah <junery115@gmail.com>
2022-09-16 02:36:10 +0100

    creating multiplication function

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * mul - multiplies 2 numbers
- * @m: first int to multiply
- * @n: second int to multiply
+ * @a: first int to multiply
+ * @b: second int to multiply
  * Return: results of multiplication
  */
 
