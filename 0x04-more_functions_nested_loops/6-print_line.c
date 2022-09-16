71dce03f6e5f31570ac5c17a520657fd722293ce
Japheth Awah <junery115@gmail.com>
2022-09-16 03:27:42 +0100

    printing lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line on screen
+ * @n: number of times character _ is printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for(i = 0; i < n; i++){
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
