ba92ef6bae40f7ef9c4ac6143fa1ff59a6eba6f6
Japheth Awah <junery115@gmail.com>
2022-09-16 02:11:50 +0100

    checking for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - functions that test a character if its uppercase or not
+ * @i: variable to be tested
+ * Return: 0
+ */
+
+int _isupper(int i)
+{
+	if (i >= 65 && i <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef main
+#define main
+
+int _isupper(int c);
+int _putchar(char c);
+
+#endif
