d543c0a0dac6acf162969edee1b667835d2d6ba7
Japheth Awah <junery115@gmail.com>
2022-09-20 02:31:14 +0100

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string reverse
+ *
+ * @s: string to be printed
+ *
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	count--;
+	while (count >= 0)
+	{
+		_putchar(s[count]);
+		count--;
+	}
+	_putchar('\n');
+}
