609645ece436e0038da70117f36ec120c1bd402e
Japheth Awah <junery115@gmail.com>
2022-09-20 02:12:59 +0100

    outputting a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - this functions prints a string
+ *
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+}
