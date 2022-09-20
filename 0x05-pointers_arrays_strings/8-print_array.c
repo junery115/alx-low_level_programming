d2ccd34a608ef6bc32348e41fdafa93678571e96
Japheth Awah <junery115@gmail.com>
2022-09-20 03:25:09 +0100

    printing values till n

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ *
+ * @a: array
+ * @n: number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
