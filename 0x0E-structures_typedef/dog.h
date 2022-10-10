67a62d6b93e246951796666373ec7e59270013e3
Japheth Awah <junery115@gmail.com>
2022-10-10 18:21:13 +0100

    creating the dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - holds dog properties
+ * @name: name of dog
+ * @age: age of dog
+ * @age: owner of dog
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
