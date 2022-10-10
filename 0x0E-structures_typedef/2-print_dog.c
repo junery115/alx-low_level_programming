ac9201822216b171d104562747df44b8054dedbc
Japheth Awah <junery115@gmail.com>
2022-10-10 19:25:53 +0100

    fixed print dog function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,12 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nill)";
+	if (d->owner == NULL)
+		d->owner = "(nill)";
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("owner: %s\n", d->owner);
