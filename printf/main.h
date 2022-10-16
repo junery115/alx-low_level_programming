5311687e64fb07be3a84dfadbe92bbd94c96c92b
Japheth Awah <junery115@gmail.com>
2022-10-16 21:50:48 +0100

    added print_str, print_char, identifiers struc

--- printf/main.h
+++ printf/main.h
@@ -3,7 +3,17 @@
 
 #include <stdarg.h>
 
+typedef struct identifiers
+{
+	char *identifier;
+	int (*printer)(va_list);
+} identifiers;
+
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_str(va_list arg);
+int print_char(va_list ard);
+
 
 
 
