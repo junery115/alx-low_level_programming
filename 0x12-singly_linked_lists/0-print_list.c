92ce18b6627fad4bb3244c1ed376fcdb3c33e4a9
Japheth Awah <junery115@gmail.com>
2022-10-21 00:37:01 +0100

    creating directory adding first file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - function to print all elements of a linked list
+ * @h: pointer to list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0](nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
