c77724dec5c806478aa2c509cc12c37323fe9e0f
Japheth Awah <junery115@gmail.com>
2022-10-21 02:26:24 +0100

    updated free_list function

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,12 +11,16 @@ void free_list(list_t *head)
 {
 	list_t *tmp;
 
-	while (head)
+	if (head)
 	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
+	while (head)
+		{
+			tmp = head->next;
+			if (head->str)
+				free(head->str);
+			free(head);
+			head = tmp;
+		}
 	}
 }
 
