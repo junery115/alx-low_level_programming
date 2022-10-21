8fb0fc672884dbe908b43cc9ad161d50206a9c43
Japheth Awah <junery115@gmail.com>
2022-10-21 02:47:07 +0100

    function that gets called before main function is called

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) print_before_main(void);
+
+/**
+ * print_before_main - prints a string before main
+ *
+ * Return: void
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and ye, you must allow,\n"
+		"I bore my house upon your back!\n");
+}
