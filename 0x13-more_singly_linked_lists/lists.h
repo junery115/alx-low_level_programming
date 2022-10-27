c62d0793a9b1cc27bfe5d6fd10219b3bc089e1e1
Japheth Awah <junery115@gmail.com>
2022-10-27 03:25:19 +0100

    finds loop in linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -40,6 +40,7 @@ size_t print_listint_safe(const listint_t *head);
 const listint_t *find_loop_print(const listint_t *meet, const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *loop_start(listint_t *meet, listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
 
 
