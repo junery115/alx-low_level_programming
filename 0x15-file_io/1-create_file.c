9ee2aaf0b36024ae307e8a9e759913ef32dc01e5
Japheth Awah <junery115@gmail.com>
2022-11-01 03:16:32 +0100

    added function to create and write to a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file name to be created
+ * @text_content: content to be written to file
+ *
+ * Return: 1 if successful and -1 if it fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len, ltr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	ltr = write(fd, text_content, len);
+
+	if (ltr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+
+
+
+}
